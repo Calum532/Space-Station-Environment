0f4dcf314f078841964e1f1a5f81f521f27bdb74
Calum <43376276+Calum532@users.noreply.github.com>
2019-04-12 11:40:50 +0100

    Roomba robots moving around with sound, audio manager imported

---
+++ Space Station Environment/Assets/Scripts/Patrol.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        transform.LookAt(moveSpots[randomSpot]);
+
+        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 5f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
