43624f97d93efa5be9f0b0b09947a716a6333084
Lee <L918Sturrock@hotmail.com>
2019-04-14 04:00:01 +0100

    Created script for spark particle prefab, randomizes audio and spark appearance chance

---
+++ Space Station Environment/Assets/Scripts/Particle Triggers/ComputerSparks.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ComputerSparks : MonoBehaviour
+{
+    private AudioSource _AudioSource;
+    public AudioClip[] sparkSounds;
+    private ParticleSystem sparksParticle;
+    readonly System.Random rnd = new System.Random();
+
+    void Awake()
+    {
+        _AudioSource = GetComponent<AudioSource>();
+        sparksParticle = gameObject.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int Chance = rnd.Next(1, 100);
+
+        GetSparkClip();
+
+        if (!_AudioSource.isPlaying && Chance == 1)
+        {
+            _AudioSource.PlayOneShot(_AudioSource.clip);
+            sparksParticle.Play();
+        }
+    }
+
+    private void GetSparkClip()
+    {
+        _AudioSource.clip = sparkSounds[Random.Range(0, sparkSounds.Length)];
+    }
+}
