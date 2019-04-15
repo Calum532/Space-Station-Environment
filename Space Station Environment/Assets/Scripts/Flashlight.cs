1e430c8c51ef155c4893c0d29cbf5acf6806d438
Calum <43376276+Calum532@users.noreply.github.com>
2019-04-15 18:15:45 +0100

    Togglable flashlight added to player

---
+++ Space Station Environment/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            FindObjectOfType<AudioManager>().Play("Flashlight");
+
+            if (GetComponent<Light>().enabled)
+            {
+                GetComponent<Light>().enabled = false;
+            }
+            else
+            {
+                GetComponent<Light>().enabled = true;
+            }
+        }
+    }
+}
