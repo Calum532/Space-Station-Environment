5f503463b95998affca1f282718c065ab352ab95
Lee <L918Sturrock@hotmail.com>
2019-04-25 03:06:27 +0100

    Hangar lift script created

---
+++ Space Station Environment/Assets/Scripts/Lift.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Lift : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
