80f8d25cf02f278f9dff5b596f02bd4a5dd706e6
Lee <L918Sturrock@hotmail.com>
2019-04-25 03:08:01 +0100

    Implemented faulty pressurization door going into research room, features alarm sound, steam particles and flickering lights

---
+++ Space Station Environment/Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    private Light light;
+    public float waitTime = 0.5f;
+
+    void Awake()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            light.enabled = !light.enabled;
+        }
+    }
+}
---
+++ Space Station Environment/Assets/Scripts/Particle Triggers/Pressurize.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Pressurize : MonoBehaviour
+{
+    private AudioSource _AudioSource;
+    public AudioClip[] steamSounds;
+    public ParticleSystem[] steamParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetSteamClip();
+
+            if (!_AudioSource.isPlaying)
+            {
+                _AudioSource.PlayOneShot(_AudioSource.clip);
+                steamParticles[0].Play();
+                steamParticles[1].Play();
+                steamParticles[2].Play();
+                steamParticles[3].Play();
+            }
+        }
+    }
+
+    private void GetSteamClip()
+    {
+        _AudioSource.clip = steamSounds[Random.Range(0, steamSounds.Length)];
+    }
+}
