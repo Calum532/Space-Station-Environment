a71236aa3c563f25f1b669ce88a2433930aaeccc
Lee <L918Sturrock@hotmail.com>
2019-04-26 05:20:01 +0100

    Made datapads functional and renamed prefab, outline doesn't work very well so may need a different way to highlight them

---
+++ Space Station Environment/Assets/Scripts/DataPad/DataPadAction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DataPadAction : MonoBehaviour
+{
+    public GameObject datapadUI;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            FindObjectOfType<AudioManager>().Play("Interact");
+            datapadUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            FindObjectOfType<AudioManager>().Play("Interact");
+            datapadUI.SetActive(false);
+        }
+    }
+}
---
+++ Space Station Environment/Assets/Scripts/DataPad/DataPadOutline.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class DataPadOutline : MonoBehaviour
+{
+    private Outline outline;
+
+    void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            outline.OutlineWidth = 10f;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            outline.OutlineWidth = 0f;
+        }
+    }
+}
