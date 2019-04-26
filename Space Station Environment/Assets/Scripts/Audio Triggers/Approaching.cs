6450c0a40c72edb7878bb653e7233f9ea856acd1
Lee <L918Sturrock@hotmail.com>
2019-04-26 05:20:28 +0100

    Audio trigger scripts and prefabs created

---
+++ Space Station Environment/Assets/Scripts/Audio Triggers/Approaching.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Approaching : MonoBehaviour
+{
+    private AudioSource _AudioSource;
+    private GameObject player;
+    private Vector3 dest;
+    public AudioClip[] approachingSounds;    
+    public float speed = 0.5f;
+
+    void Awake()
+    {
+        _AudioSource = transform.GetChild(0).GetComponent<AudioSource>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        dest = player.transform.position;        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {            
+            PlayApproachingCreatureAudio();
+            Destroy(gameObject, 13f);
+        }
+    }
+
+    public void PlayApproachingCreatureAudio()
+    {
+        ApproachingClip();
+        if (!_AudioSource.isPlaying)
+        {
+            _AudioSource.PlayOneShot(_AudioSource.clip);
+            MoveToPlayer();
+        }
+    }
+
+    void MoveToPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, dest, speed);
+    }
+
+    private void ApproachingClip()
+    {
+        _AudioSource.clip = approachingSounds[Random.Range(0, approachingSounds.Length)];
+    }
+}
---
+++ Space Station Environment/Assets/Scripts/Audio Triggers/Regular.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Regular : MonoBehaviour
+{
+    private AudioSource _AudioSource;
+
+    public AudioClip[] regularSounds;
+
+    void Awake()
+    {
+        _AudioSource = transform.GetChild(0).GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayRegularCreatureAudio();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayRegularCreatureAudio()
+    {
+        RegularClip();
+        if (!_AudioSource.isPlaying)
+        {
+            _AudioSource.PlayOneShot(_AudioSource.clip);
+        }
+    }
+
+    private void RegularClip()
+    {
+        _AudioSource.clip = regularSounds[Random.Range(0, regularSounds.Length)];
+    }
+}
