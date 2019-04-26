26cfc0d5ffb0eded7a251a0b5d020979eaa9b895
Lee <L918Sturrock@hotmail.com>
2019-04-26 08:12:56 +0100

    Lift sound added and applied, lift error sound and script added and applied

---
+++ Space Station Environment/Assets/Scripts/LiftError.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class LiftError : MonoBehaviour
+{
+    private AudioSource _as;
+    private float nextActionTime;
+    public float period = 5f;
+
+    void Awake()
+    {
+        _as = GetComponent<AudioSource>();
+    }
+
+
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime = Time.time + period;
+            // execute block of code here
+            _as.Play();
+        }
+    }
+}
